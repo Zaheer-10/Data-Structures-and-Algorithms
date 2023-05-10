70690c7669d20156f0703c5a55bcb0a5276dd906
Zaheer <112798052+Zaheer-10@users.noreply.github.com>
2023-05-10 12:28:43 +0530

    added DLL

---
+++ Double_LinkedList/insert@spec_Pos.py
@@ -0,0 +1,64 @@
+class Node :
+    def __init__ (self,data):
+        self.data = data
+        self.next = None
+        self.previous = None
+        
+class DLL:
+    def __init__(self):
+        self.head = None
+            
+    def Insert_Position(self,data,position):
+        n = Node(data)
+        temp = self.head 
+        
+        for i in range(1,position-1):
+            temp = temp.next
+        n.previous = temp
+        n.next = temp.next
+        temp.next.previous = n
+        temp.next = n
+        
+        
+    def traverse (self):
+        if self.head is None:
+            print("Empty list ")
+        
+        else:
+            
+            temp = self.head
+            while temp:
+                print(temp.data ,'-->',end='')
+                temp = temp.next
+                
+                
+n = Node(101)
+n1 = Node(102)
+n2= Node(103)
+n3= Node(10644)
+n4= Node(1454)
+n5 = Node(451)
+
+L =DLL()
+
+L.head = n
+
+n.next = n1
+
+n1.next = n2
+n1.previous = n
+
+n2.next = n3
+n2.previous = n1
+
+n3.next = n4
+n3.previous = n2
+
+n4.next = n5
+n4.previous = n3
+
+L.Insert_Position(466,2)
+# 313 -->466 -->101 -->102 -->103 -->10644 -->1454 -->451 -->
+
+L.traverse()
+# 101 -->102 -->103 -->10644 -->1454 -->451 -->
